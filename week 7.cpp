0be7ffceae32bd336f89f4344b16359446543b66
Jirapat Kaewsongsang <60883386+keedjk7@users.noreply.github.com>
2020-11-22 23:23:39 +0700

    Update week 7.cpp

--- week 7.cpp
+++ week 7.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int row,colum;
 
 void set_position(char *ch,int a,int b){
-	if(a==0||b==0||a==row-1||b==colum-1){
+	if(a==1||b==1||a==row||b==colum){
 		*ch='X';
 	}
 	else if((a+b)%2==0){
@@ -29,14 +29,14 @@ int main(){
 	
 	char array[row][colum];
 	
-	for(int i=0;i<row;i++){
-		for(int j=0;j<colum;j++){
+	for(int i=1;i<=row;i++){
+		for(int j=1;j<=colum;j++){
 			set_position(&array[i][j],i,j);
 		}
 	}
 	
-	for(int i=0;i<row;i++){
-		for(int j=0;j<colum;j++){
+	for(int i=1;i<=row;i++){
+		for(int j=1;j<=colum;j++){
 			Draw(&array[i][j]);
 		}
 		cout<<endl;
